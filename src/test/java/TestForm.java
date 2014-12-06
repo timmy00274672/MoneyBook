34a76e1589b9ebaad230496d406935b0f094af0b
Yu-Ting Chen <timmy00274672@gmail.com>
2014-12-06 11:44:06 +0800

    Add the start form event and test for our money book process.

---
+++ src/test/java/TestForm.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import org.activiti.engine.FormService;
+import org.activiti.engine.test.ActivitiRule;
+import org.activiti.engine.test.Deployment;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class TestForm {
+	@Rule
+	public ActivitiRule activitiRule = new ActivitiRule("activiti.cfg-mem.xml");
+	
+	@Test
+	@Deployment(resources = { "diagrams/MyProcess.bpmn" })
+	public void TestFormStartEvent(){
+		FormService formService = activitiRule.getFormService();
+		String processDefId = activitiRule.getRepositoryService().createProcessDefinitionQuery().singleResult().getId();
+		
+		Map<String, String> properties = new HashMap<String, String>();
+		properties.put("name", "timmy");
+		properties.put("email", "timmy00274672@gmail.com");
+		properties.put("category", "food");
+		properties.put("amount", "100");
+		formService.submitStartFormData(processDefId, properties);
+	}
+}
